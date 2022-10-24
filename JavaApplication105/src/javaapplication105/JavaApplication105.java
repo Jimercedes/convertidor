89cae7b72bf7cc7016ffd79d58e7eeee88965448
Juan Mercedes <juanmercedes22@gmail.com>
2022-10-23 20:14:15 -0400

    system

--- JavaApplication105/src/javaapplication105/JavaApplication105.java
+++ JavaApplication105/src/javaapplication105/JavaApplication105.java
@@ -14,6 +14,7 @@ public class JavaApplication105 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("klk");
         // TODO code application logic here
     }
     
